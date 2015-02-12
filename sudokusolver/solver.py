40f177ac60c6c932a70e4d1a281d3ce89a3b81fb
Ondrej Skopek <skopekondrej@gmail.com>
2015-02-12 09:19:43 +0100

    Fixes #3

--- sudokusolver/solver.py
+++ sudokusolver/solver.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import sudokusolver.sudoku_inet as inet
+from sudokusolver.common import from_vals, to_grid, to_str
+
 # TODO implement this
 def solve_backtrack(sudoku):
     for i in range(len(sudoku)):
@@ -14,29 +17,21 @@ def solve_backtrack(sudoku):
 # Reference implementation
 # See: http://norvig.com/sudoku.html
 def solve_inet(sudoku):
-    def from_vals(vals):
-        sudoku = []
-        for i in range(9):
-            line = []
-            for j in range(9):
-                line.append(int(vals[str(chr(ord('A') + i)) + str(j + 1)]))
-            sudoku.append(line)
-        return sudoku
-
-    def to_grid(sudoku):
-        grid = ''
-        for i in range(len(sudoku)):
-            for j in range(len(sudoku[i])):
-                if sudoku[i][j] == 0:
-                    grid = grid + '.'
-                else:
-                    grid = grid + str(sudoku[i][j])
-        return grid
-
-    import sudokusolver.sudoku_inet as inet
-
     grid = to_grid(sudoku)
     vals = inet.solve(grid)
     #inet.display(vals)
     return from_vals(vals)
 
+easy = [
+            [1, 2, 0, 0, 0, 9, 0, 8, 0],
+            [3, 0, 4, 8, 0, 0, 9, 0, 2],
+            [0, 8, 9, 0, 0, 0, 7, 0, 0],
+            [0, 4, 0, 0, 1, 5, 8, 0, 0],
+            [8, 1, 0, 0, 0, 0, 3, 0, 9],
+            [7, 0, 0, 0, 3, 8, 0, 0, 4],
+            [4, 0, 0, 7, 0, 0, 5, 3, 0],
+            [2, 0, 0, 0, 0, 0, 0, 4, 1],
+            [0, 0, 5, 0, 8, 0, 2, 0, 7]
+        ]
+solution = solve_inet(easy)
+print(to_str(solution))
