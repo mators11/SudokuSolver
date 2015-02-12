a802698d8efd992f4ab419bdd10fd3f149b2b9f7
mators11 <mr.mators11@gmail.com>
2015-02-12 14:14:53 +0100

    Fixed finder for 2.4.10 version of OpenCV

--- sudokusolver/finder.py
+++ sudokusolver/finder.py
@@ -5,13 +5,25 @@ def pre_process(path):
     img = cv2.imread(path)
     img = cv2.resize(img, (500, 500))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blr = cv2.GaussianBlur(gray, (9, 9), 0)
+    blr = cv2.GaussianBlur(gray, (3, 3), 0)
     return blr
+
 
 def find_square(image):
     thresh = cv2.adaptiveThreshold(image, 255, 1, 1, 5, 2)
-    thresh, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    biggest = find_biggest(contours)
+    
 
+    cv2.drawContours(image, biggest, -1, (255, 255, 255), 10)
+    cv2.namedWindow('Output', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('Output', image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    return image
+
+def find_biggest(contours):
     biggest = None
     max_area = 0
     for i in contours:
@@ -22,15 +34,7 @@ def find_square(image):
             if area > max_area and len(approx) == 4:
                 biggest = approx
                 max_area = area
-
-    cv2.drawContours(image, biggest, -1, (255, 255, 255), 3)
-    cv2.namedWindow('Output', cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('Output', image)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
-    return image
-
+    return biggest
 
 if __name__ == "__main__":
     test_image = pre_process('../img/test_sudoku1.jpg')
